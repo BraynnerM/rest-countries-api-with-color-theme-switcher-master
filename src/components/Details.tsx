f6955ad9ffb6ab0063acf7460b932f34c3cc63f7
Braynner Marques <braynnermarques@hotmail.com>
2023-07-06 12:42:06 -0300

    tentativa 5 deploy

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { HiOutlineArrowNarrowLeft } from "react-icons/Hi";
+import { HiOutlineArrowNarrowLeft } from "react-icons/Hi/index.ts";
 
 import "../styles/components/details.sass"
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { HiOutlineMoon } from "react-icons/Hi";
+import { HiOutlineMoon } from "react-icons/Hi/index.ts";
 
 import "../styles/components/header.sass"
 
